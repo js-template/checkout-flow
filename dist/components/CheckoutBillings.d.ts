bba55bed7f1b9c2dd2ef64863a014f875c1dfe65
Zelal Hossain <robi@ojjomedia.com>
2023-01-24 13:54:55 +0600

    dist removed

--- dist/components/CheckoutBillings.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-interface CheckoutBillingsProps {
-    user?: any;
-}
-declare const CheckoutBillings: ({ user }: CheckoutBillingsProps) => JSX.Element;
-export default CheckoutBillings;
-//# sourceMappingURL=CheckoutBillings.d.ts.map
--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "./components/checkout";
-//# sourceMappingURL=index.d.ts.map
